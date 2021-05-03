16f534e5d08d03ac7143054c70668b7f2a72bd70
Chobo666 <chwistekkaja@gmail.com>
2021-05-03 10:04:30 +0200

    init

---
+++ hexMath.js
@@ -0,0 +1,12 @@
+
+
+const eps = 0.000001; //due to double type errors
+
+function distance(A, B){ //hexes -> their distance
+    if((A.Q-B.Q)*(A.R-B.R) >= 0-eps){
+        return Math.max(Math.abs(A.Q-B.Q), Math.abs(A.R-B.R));
+    } else {
+        return Math.abs(A.Q-B.Q)+abs(A.R-B.R);
+    }
+}
+
