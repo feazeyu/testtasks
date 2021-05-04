d1226191dc822812e03dc6219de9e37f66343977
Chobo666 <chwistekkaja@gmail.com>
2021-05-04 11:41:19 +0200

    Entry

--- index.js
+++ index.js
@@ -51,57 +51,89 @@ class Hex {
     this.HarvestValue = CellDefinitionsDict[this.id].HarvestValue;
   }
 }
-<<<<<<< HEAD
 
 class Entry {
-  constructor(data, createMsgFnc, id) {
+  constructor(data, createMsgFnc, channel) {
     this.data = data;
     this.createMsgFnc = createMsgFnc;
-    this.id = id;
+    this.channel = channel;
   }
   sendMsg() {
     let msg = this.createMsgFnc(this.data);
-    message.channel.send(msg).then(() =>
-      channel.messages.fetch({ limit: 1 }).then((messages) => {
+    this.channel.send(msg).then(() =>
+      this.channel.messages.fetch({ limit: 1 }).then((messages) => {
         let lastMessage = messages.first();
         lastMessage.react("◀️").then(() => lastMessage.react("▶️"));
+        this.id = lastMessage.id;
+        console.log(this.id);
       })
     );
   }
-  editMsg(){
+  editMsg() {
     let msg = this.createMsgFnc(this.data);
     //TODO
   }
-  scrollForward(){
+  scrollForward() {
     this.data.pages.page++;
-    if(this.data.pages.page > this.data.pages.limit){
+    if (this.data.pages.page > this.data.pages.limit) {
       this.data.pages.page = 0;
     }
     this.editMsg();
   }
-  scrollBackwards(){
+  scrollBackwards() {
     this.data.pages.page--;
-    if(this.data.pages.page < 0){
+    if (this.data.pages.page < 0) {
       this.data.pages.page = this.data.pages.limit;
     }
     this.editMsg();
   }
 }
 
-=======
-client.on('messageReactionAdd', (reaction, user) => {
-  let message = reaction.message, emoji = reaction.emoji;
-  if (user.bot){
+client.on("messageReactionAdd", (reaction, user) => {
+  let message = reaction.message,
+    emoji = reaction.emoji;
+  if (user.bot) {
     return 0;
   }
-  if (emoji.name == '◀️') {
-          console.log("Back at message: " + message.id)          
-  }
-  else if (emoji.name == '▶️') {
-          console.log("Forward at message: " + message.id)
+  if (emoji.name == "◀️") {
+    console.log("Back at message: " + message.id);
+  } else if (emoji.name == "▶️") {
+    console.log("Forward at message: " + message.id);
   }
 });
->>>>>>> f67beeeb8cc1453e4bc46d393bdc7a2e093d7f6d
+
+function createBestSpotsMsg(data) {
+  let spots = [];
+  let begin = pageSize * data.pages.page;
+  let end = Math.min(pageSize * (data.pages.page + 1), data.maxEntries);
+  //console.log(begin);
+  //console.log(end);
+  for (x = begin; x < end; x++) {
+    spots.push({
+      name: (x + 1) + ". " + data.harvest[x].coords.gotoCoords(),
+      value:
+        data.harvest[x].LQ +
+        "<:Salute1:786442517209415710> " +
+        data.harvest[x].MR +
+        "<:Metal:757976643493953688> " +
+        data.harvest[x].GR +
+        "<:Gas:757976643204546618> " +
+        data.harvest[x].CR +
+        "<:Crystal:757976643363930122>" +
+        " | Total: " +
+        data.harvest[x].total +
+        " | Distance: " +
+        data.harvest[x].dist,
+    });
+  }
+  //console.log(spots);
+  return new Discord.MessageEmbed()
+  .setColor("#0099ff")
+  .setTitle("Best resource spots page " + (data.pages.page+1) + "/" + data.pages.limit + ":")
+  .setDescription("Fields, Moons and planets for radius: " + data.radius)
+  .addFields(spots);
+}
+
 client.on("message", (message) => {
   let args = message.content.split(" ");
   let harvest;
@@ -109,8 +141,10 @@ client.on("message", (message) => {
     //try {
     if (args.length > 1) {
       switch (args[1].toLowerCase()) {
-<<<<<<< HEAD
-        case "rss":
+        case "rss": //RSS command
+          if (args[6] == undefined) { // default size 
+            args[6] = 50;
+          }
           harvest = bestTotalSpots(
             rssAt,
             new hexMath.Coords(args[2], args[3]),
@@ -118,77 +152,61 @@ client.on("message", (message) => {
             args[5],
             args[6]
           );
-=======
-        case "rss":                            //RSS command
-          harvest = bestTotalSpots(rssAt, new hexMath.Coords(args[2], args[3]), args[4], args[5], args[6])
->>>>>>> f67beeeb8cc1453e4bc46d393bdc7a2e093d7f6d
           //TODO Osetrit crashe pri nezadani argumentu
           if (parseInt(args[4]) > 10) {
             message.channel.send(tooBigRadiusError(parseInt(args[4])));
             break;
           }
-<<<<<<< HEAD
-          var temp = [];
+          let new_entry = new Entry({
+            "harvest": harvest,
+            "radius": args[4],
+            "pages":{
+              "page": 0,
+              "limit": Math.ceil(harvest.length/pageSize)
+            },
+            "maxEntries": harvest.length
+          }, createBestSpotsMsg, message.channel);
+          new_entry.sendMsg();
+          /*
+          var pages = [];
           if (args[6] == undefined) {
             args[6] = 10;
           }
-          for (x = 0; x < args[6]; x++) {
-            temp.push({
-              name: x + 1 + ". " + harvest[x].coords.gotoCoords(),
-              value:
-                harvest[x].LQ +
-                "<:Salute1:786442517209415710> " +
-                harvest[x].MR +
-                "<:Metal:757976643493953688> " +
-                harvest[x].GR +
-                "<:Gas:757976643204546618> " +
-                harvest[x].CR +
-                "<:Crystal:757976643363930122>" +
-                " | Total: " +
-                harvest[x].total +
-                " | Distance: " +
-                harvest[x].dist,
-            });
+          let iters = 0;
+          for (i = 0; i < Math.ceil(args[6] / pageSize); i++) {
+            pages.push([]);
+            for (x = 0; x < pageSize && iters < args[6]; x++) {
+              pages[i].push({
+                name: iters + 1 + ". " + harvest[x].coords.gotoCoords(),
+                value:
+                  harvest[x].LQ +
+                  "<:Salute1:786442517209415710> " +
+                  harvest[x].MR +
+                  "<:Metal:757976643493953688> " +
+                  harvest[x].GR +
+                  "<:Gas:757976643204546618> " +
+                  harvest[x].CR +
+                  "<:Crystal:757976643363930122>" +
+                  " | Total: " +
+                  harvest[x].total +
+                  " | Distance: " +
+                  harvest[x].dist,
+              });
+              iters++;
+            }
           }
           let msg = new Discord.MessageEmbed()
             .setColor("#0099ff")
             .setTitle("Best resource spots:")
             .setDescription("Fields, Moons and planets for radius: " + args[4])
-            .addFields(temp);
+            .addFields(pages[1]);
           message.channel.send(msg).then(() =>
             channel.messages.fetch({ limit: 1 }).then((messages) => {
               let lastMessage = messages.first();
               lastMessage.react("◀️").then(() => lastMessage.react("▶️"));
-              //TODO Strankovani pres reakce
-            })
-          );
-=======
-         var pages = [];
-         if(args[6] == undefined){
-           args[6] = 10
-         }
-         let iters = 0;
-         for(i = 0; i < Math.ceil(args[6]/pageSize);i++){
-           pages.push([]);
-          for(x = 0; x < pageSize && iters < args[6]; x++){
-              pages[i].push({name:iters+1+". " + harvest[x].coords.gotoCoords(), value: harvest[x].LQ + "<:Salute1:786442517209415710> " + harvest[x].MR + "<:Metal:757976643493953688> " +harvest[x].GR + "<:Gas:757976643204546618> " + harvest[x].CR + "<:Crystal:757976643363930122>" + " | Total: " + harvest[x].total + " | Distance: " + harvest[x].dist})
-              iters++;
-            }
-        }
-         let msg = new Discord.MessageEmbed()
-	          .setColor('#0099ff')
-          	.setTitle('Best resource spots:')
-          	.setDescription('Fields, Moons and planets for radius: ' + args[4])
-            .addFields(
-              pages[1]
-         )
-          message.channel.send(msg).then(() =>
-            channel.messages.fetch({ limit: 1 }).then(messages => {
-            let lastMessage = messages.first();
-              lastMessage.react('◀️').then(() => lastMessage.react('▶️'));
               //TODO Reaction pages
-          }));
->>>>>>> f67beeeb8cc1453e4bc46d393bdc7a2e093d7f6d
+            })
+          );*/
           break;
         case "labor":
           harvest = laborAt(
@@ -425,7 +443,7 @@ function bestTotalSpots(fnc, middle, radius, distance, entries = 50) {
     }
   }
   spots.sort(comparatorTotal);
-  return spots.slice(0, entries);
+  return spots.slice(0, Math.min(entries, spots.length));
 }
 
 function rssAt(coords, radius) {
