0a9aac6f95c9b226a816be9a40416ff0d6d7f0b3
feazeyu <hernifea@seznam.cz>
2021-05-05 11:13:18 +0200

    not much

---
+++ stationPlanner.js
@@ -0,0 +1,18 @@
+class Station{
+    constructor(q, r){
+        this.q = q;
+        this.r = r;
+        this.template = "MF MF TP HSA";
+        this.MPL = true; //Is mars prosper league?
+        this.FL = true; //Is frontier legion?
+        this.NH = true; //Can it have new horizons?
+        this.stationHarvest = 1;
+    }
+
+    calculateStationHarvest(){
+        if(this.FL){
+
+        }
+
+    }
+}
